499a9190296f0b2f47f95b1a1bf7ad48fd3524b6
univanxx <wchhiaarid@yandex.ru>
2022-06-08 18:10:40 +0000

    Modified (simplified) model

--- MyModel.py
+++ MyModel.py
@@ -5,20 +5,18 @@ from MyPooling import LogSumExpPool
 
 
 class MyResNet(nn.Module):
-    
+    # Realization from https://arxiv.org/abs/1705.02315
     def __init__(self, r=5):
         super(MyResNet, self).__init__()
         
-        self.pretrained = models.resnet50(pretrained=True)
-        self.pretrained.fc = nn.Identity()
-        self.pretrained.avgpool = nn.Identity()
+        self.pretrained = nn.Sequential(*list(models.resnet50(pretrained=True).children())[:-2])
         self.transition = nn.Conv2d(2048, 2048, kernel_size=(2, 2), padding=16, bias=False)
         self.pooling = LogSumExpPool(r=r)
         self.prediction = nn.Linear(in_features=2048, out_features=8, bias=True)
     
     def forward(self, x):
         x = self.pretrained(x)
-        x = self.transition(x[:,:,None,None])
+        x = self.transition(x)
         x = self.pooling(x)
         x = self.prediction(torch.flatten(x, start_dim=1))
         return x
--- MyPooling.py
+++ MyPooling.py
@@ -7,7 +7,7 @@ class LogSumExpPool(nn.Module):
     def __init__(self, r = 5):
         super(LogSumExpPool, self).__init__()
         self.r = r
-    # Realization from https://arxiv.org/abs/1705.02315
+    # Log-Sum-Exp Padding realization from https://arxiv.org/abs/1705.02315
     def forward(self, x):
         max_vals = torch.amax(torch.abs(x), dim = (2,3))
         x = max_vals + (1/self.r)*torch.log((1/x.shape[-1])*torch.sum(torch.exp(self.r*(x - max_vals[:,:,None,None])), dim=(2,3)))
