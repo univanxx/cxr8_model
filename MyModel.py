b5f19ed4ae9cd8185d8a9d48170339f8412f1afe
univanxx <wchhiaarid@yandex.ru>
2022-06-09 11:36:06 +0000

    Modified model output for MIMIC classes

--- MyModel.py
+++ MyModel.py
@@ -6,13 +6,14 @@ from MyPooling import LogSumExpPool
 
 class MyResNet(nn.Module):
     # Realization from https://arxiv.org/abs/1705.02315
-    def __init__(self, r=5):
+    def __init__(self, n_classes, r=5):
         super(MyResNet, self).__init__()
         
         self.pretrained = nn.Sequential(*list(models.resnet50(pretrained=True).children())[:-2])
         self.transition = nn.Conv2d(2048, 2048, kernel_size=(2, 2), padding=16, bias=False)
         self.pooling = LogSumExpPool(r=r)
-        self.prediction = nn.Linear(in_features=2048, out_features=8, bias=True)
+        # We use classes including 'No Finding' label!
+        self.prediction = nn.Linear(in_features=2048, out_features=n_classes, bias=True)
     
     def forward(self, x):
         x = self.pretrained(x)
