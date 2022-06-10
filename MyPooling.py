2fecd5f9aacda02001460e7d0ef2b02afd531d96
univanxx <wchhiaarid@yandex.ru>
2022-06-10 15:47:49 +0000

    Pooling bug fixed

--- MyPooling.py
+++ MyPooling.py
@@ -10,5 +10,5 @@ class LogSumExpPool(nn.Module):
     # Log-Sum-Exp Padding realization from https://arxiv.org/abs/1705.02315
     def forward(self, x):
         max_vals = torch.amax(torch.abs(x), dim = (2,3))
-        x = max_vals + (1/self.r)*torch.log((1/x.shape[-1])*torch.sum(torch.exp(self.r*(x - max_vals[:,:,None,None])), dim=(2,3)))
+        x = max_vals + (1/self.r)*torch.log(1e-7 + (1/x.shape[-1])*torch.sum(torch.exp(self.r*(x - max_vals[:,:,None,None])), dim=(2,3)))
         return x
